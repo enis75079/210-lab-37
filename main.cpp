0935f1bd997a22dd665a9632d6563ea660e8fcb7
enis75079 <naveenislam55@gmail.com>
2024-12-05 10:00:55 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -16,11 +16,15 @@ void part_Two();
 
 int main() {
     // user input string
-    string userString = "";
+    /*string userString = "";
     cout << "Enter a string (no spaces): ";
     cin >> userString;
     cout << "Ascii sum of " << userString << " is: " << sum_ascii(userString) << endl; 
-    return 0;
+    return 0;*/
+
+    // calls the part two function
+    part_Two();
+
 }
 
 // sum_ascii function. receives a single string and returns the sum of that string's character's ASCII values
@@ -31,19 +35,21 @@ int sum_ascii(const string& userInput) {
         sum += static_cast<int>(x);
     }
     return sum;
+
 }
 
+// part two function. reads the lab-37-data.txt file and totals the ascii value of all the strings
 void part_Two() {
     ifstream file("lab-37-data.txt");
     string txtLine;
-    int total = 0;
+    long long total = 0;
     if (!file.is_open()) {
         cout << "Could not open file. Please try again." << endl;
         return;
     }
 
     while (getline(file, txtLine)) {
-        total = sum_ascii(txtLine);
+        total += sum_ascii(txtLine);
     }
     file.close();
     cout << "Total of lab-37-data.txt ASCII values: " << total << endl;
