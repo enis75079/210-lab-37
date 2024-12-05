8a075a2143991f45a1306c4db7c270105abd5e5d
enis75079 <naveenislam55@gmail.com>
2024-12-05 09:41:06 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -13,16 +13,18 @@ using namespace std;
 int sum_ascii(string&);
 
 int main() {
+    // user input string
     string userString = "";
     cout << "Enter a string (no spaces): ";
     cin >> userString;
-    cout << "\nAscii sum of " << userString << " is: " << sum_ascii(userString) << endl; 
+    cout << "Ascii sum of " << userString << " is: " << sum_ascii(userString) << endl; 
     return 0;
 }
 
 // sum_ascii function. receives a single string and returns the sum of that string's character's ASCII values
 int sum_ascii(string& userInput) {
     int sum;
+    // reads every character from the user's string and adds the ascii value to the sum
     for (char x : userInput) {
         sum += static_cast<int>(x);
     }
