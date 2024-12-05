56c77072e228eade29b0c5adef3ea6b4f0f6ab24
enis75079 <naveenislam55@gmail.com>
2024-12-05 09:36:22 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@ Naveen Islam
 #include <string>
 using namespace std;
 
+// function prototypes
+int sum_ascii(string&);
+
 int main() {
-    
+    return 0;
+}
+
+// sum_ascii function. receives a single string and returns the sum of that string's character's ASCII values
+int sum_ascii(string& userInput) {
+    int sum;
+    for (char x : userInput) {
+    }
 }
